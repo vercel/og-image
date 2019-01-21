29b9603c2267a924f5bf0c8b708b25f79fdc2673
Steven <steven@ceriously.com>
2019-01-21 11:07:35 -0500

    Remove unused file dev.js

--- dev.js
+++
@@ -1,5 +0,0 @@
-const { createServer } = require('http');
-const PORT = 3000;
-const handleServer = require('./card');
-const handleListen = () => console.log(`Listening on ${PORT}...`);
-createServer(handleServer).listen(PORT, handleListen);
