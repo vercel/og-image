8ad4e882e5c7a5159caea6e7a78d9abe267c06c3
Steven <steven@ceriously.com>
2019-01-22 09:47:39 -0500

    Add better DX

--- card.ts
+++ card.ts
@@ -1,14 +1,23 @@
-import { IncomingMessage, ServerResponse } from 'http';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
 import { parseRequest } from './parser';
 import { getScreenshot } from './chromium';
 import { getHtml } from './template';
 import { writeTempFile, pathToFileURL } from './file';
 
-async function handler(req: IncomingMessage, res: ServerResponse) {
+const isDev = !process.env.NOW_REGION;
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
     try {
         const { type, text, fontWeight, fontSize, images } = parseRequest(req);
         const html = getHtml(text, fontWeight, fontSize, images);
         const filePath = await writeTempFile(text, html);
+        
+        if (isDev) {
+            res.setHeader('Content-Type', 'text/html');
+            res.end(html);
+            return;
+        }
+
         const fileUrl = pathToFileURL(filePath);
         const file = await getScreenshot(fileUrl, type);
         res.statusCode = 200;
@@ -21,14 +30,11 @@ async function handler(req: IncomingMessage, res: ServerResponse) {
         res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
         console.error(e.message);
     }
-};
+}
 
 
-if (!process.env.NOW_REGION) {
-    const { createServer } = require('http');
+if (isDev) {
     const PORT = process.env.PORT || 3000;
     const listen = () => console.log(`Listening on ${PORT}...`);
     createServer(handler).listen(PORT, listen);
 }
-
-module.exports = handler;
