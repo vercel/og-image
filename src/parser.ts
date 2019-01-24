32877368724a1388b972f26713caf55e86327969
Steven <steven@ceriously.com>
2019-01-24 17:40:38 -0500

    Add dark theme

--- src/parser.ts
+++ src/parser.ts
@@ -4,10 +4,15 @@ import { parse } from 'url';
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
     const { pathname = '/', query = {} } = parse(req.url || '', true);
-    const { fontSize, images, md } = query;
+    const { fontSize, images, theme, md } = query;
     if (Array.isArray(fontSize)) {
         throw new Error('Expected a single fontSize');
     }
+    if (Array.isArray(theme)) {
+        throw new Error('Expected a single theme');
+    }
+
+    
     const arr = pathname.slice(1).split('.');
     let extension = '';
     let text = '';
@@ -19,15 +24,24 @@ export function parseRequest(req: IncomingMessage) {
         extension = arr.pop() as string;
         text = arr.join('.');
     }
-    
+
     const parsedRequest: ParsedRequest = {
         type: extension === 'jpeg' ? extension : 'png',
         text: decodeURIComponent(text),
+        theme: theme === 'dark' ? 'dark' : 'light',
         md: md === '1' || md === 'true',
         fontSize: fontSize || '75px',
-        images: Array.isArray(images) && images.length > 0
-            ? images
-            : ['https://assets.zeit.co/image/upload/front/assets/design/now-black.svg'],
+        images: Array.isArray(images) ? images : [images],
     };
+    parsedRequest.images = getDefaultImages(parsedRequest.images, parsedRequest.theme);
     return parsedRequest;
+}
+
+function getDefaultImages(images: string[], theme: Theme) {
+    if (images.length > 0 && /^http/.test(images[0])) {
+        return images;
+    }
+    return theme === 'light'
+    ? ['https://assets.zeit.co/image/upload/front/assets/design/now-black.svg']
+    : ['https://assets.zeit.co/image/upload/front/assets/design/now-white.svg'];
 }
