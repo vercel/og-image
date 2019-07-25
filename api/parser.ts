ca583e770c777b53bb95f6ceb5dc349026758127
Steven <steven@ceriously.com>
2019-07-25 10:52:13 -0400

    Fix types

--- api/parser.ts
+++ api/parser.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage } from 'http';
 import { parse } from 'url';
+import { ParsedRequest, Theme } from './types';
 
 export function parseRequest(req: IncomingMessage) {
     console.log('HTTP ' + req.url);
--- api/template.ts
+++ api/template.ts
@@ -2,6 +2,7 @@
 import { readFileSync } from 'fs';
 import marked from 'marked';
 import { sanitizeHtml } from './sanitizer';
+import { ParsedRequest } from './types';
 const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
