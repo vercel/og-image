18620a577234d9eeabd76f235048b82a25158a36
Steven <steven@ceriously.com>
2020-04-28 10:02:23 -0400

    Update image name

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -52,8 +52,8 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
     const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/zeit-black-triangle.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/zeit-white-triangle.svg';
+        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
+        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
 
     if (!images || !images[0]) {
         return [defaultImage];
--- web/index.ts
+++ web/index.ts
@@ -142,14 +142,14 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/zeit-black-triangle.svg' },
+    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
 
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/zeit-white-triangle.svg' },
+    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
 ];
