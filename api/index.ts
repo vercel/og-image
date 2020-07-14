ab337e74aee3b2b48591f883f1a5bbc6052c9158
Steven <steven@ceriously.com>
2020-07-14 18:19:46 -0400

    Fix dev check

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { parseRequest } from './_lib/parser';
 import { getScreenshot } from './_lib/chromium';
 import { getHtml } from './_lib/template';
 
-const isDev = process.env.VERCEL_REGION === 'dev1';
+const isDev = !process.env.AWS_REGION;
 const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
 
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
