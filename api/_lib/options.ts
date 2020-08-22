ce475b511848db13806d42151ca341cebc717c54
Wei He <git@weispot.com>
2020-08-21 21:16:00 -0400

    style: set up eslint

--- api/_lib/options.ts
+++ api/_lib/options.ts
@@ -1,9 +1,9 @@
 import chrome from 'chrome-aws-lambda';
 const exePath = process.platform === 'win32'
-? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-: process.platform === 'linux'
-? '/usr/bin/google-chrome'
-: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+    : process.platform === 'linux'
+        ? '/usr/bin/google-chrome'
+        : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 interface Options {
     args: string[];
@@ -23,7 +23,7 @@ export async function getOptions(isDev: boolean) {
         options = {
             args: chrome.args,
             executablePath: await chrome.executablePath,
-            headless: chrome.headless,
+            headless: chrome.headless
         };
     }
     return options;
