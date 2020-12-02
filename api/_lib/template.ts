cf038aeef0452e83ad6989ddc24b3e3cfbbafa03
Sarthak Kundra <sarthakkundra21@gmail.com>
2020-12-02 16:41:10 +0530

    Changed font, final CSS changes

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,45 +7,39 @@ const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+const rglr = readFileSync(`${__dirname}/../_fonts/Web-Serveroff.otf`).toString('base64');
+
 
 function getCss(theme: string, fontSize: string) {
     let background = 'white';
     let foreground = 'black';
-    let radial = 'lightgray';
+    let radial1 =  '3ECC5F';
+    let radial2 = 'f9f900';
 
     if (theme === 'dark') {
         background = 'black';
         foreground = 'white';
-        radial = 'dimgray';
     }
     return `
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
         font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('otf');
     }
 
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
         font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
     }
 
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
+  
 
     body {
         background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
+        background-image: radial-gradient(circle at 25px 25px, #${radial1} 2%, transparent 0%), radial-gradient(circle at 75px 75px, #${radial2} 2%, transparent 0%);
         background-size: 100px 100px;
         height: 100vh;
         display: flex;
@@ -100,7 +94,16 @@ function getCss(theme: string, fontSize: string) {
         font-style: normal;
         color: ${foreground};
         line-height: 1.8;
-    }`;
+    }
+    
+    .sub-heading {
+        font-family: 'Inter', sans-serif;
+        font-size: 50px;
+        font-style: normal;
+        color: ${foreground};
+        line-height: 1;
+    }
+    `;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
@@ -126,7 +129,7 @@ export function getHtml(parsedReq: ParsedRequest) {
                 md ? marked(text) : sanitizeHtml(text)
             )}
             </div>
-            <h1> Docusaurus - ${imageType} </h1>
+            <h1 class="sub-heading"> Docusaurus - ${imageType} </h1>
         </div>
     </body>
 </html>`;
