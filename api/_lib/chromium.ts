7d879675da4161c3198261fe1944e667087104b5
Steven <steven@ceriously.com>
2021-01-09 16:28:05 -0500

    Fix puppeteer import (#145)
    TypeError: Cannot read property '_launcher' of undefined

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -1,14 +1,14 @@
-import { launch, Page } from 'puppeteer-core';
+import core from 'puppeteer-core';
 import { getOptions } from './options';
 import { FileType } from './types';
-let _page: Page | null;
+let _page: core.Page | null;
 
 async function getPage(isDev: boolean) {
     if (_page) {
         return _page;
     }
     const options = await getOptions(isDev);
-    const browser = await launch(options);
+    const browser = await core.launch(options);
     _page = await browser.newPage();
     return _page;
 }
