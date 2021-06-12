d1194c5d86af7bf41b4239e355abde3c8b4a5fc9
Nico Bachner <yo@nicob.dev>
2021-06-12 18:10:18 +0200

    fix(style): fixed broken styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.tsx'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   presets: [require('tailwind-preset')],
   theme: {
-    boxShadow: {
-      sm: '0 1px 4px rgba(0, 0, 0, 0.05)',
-      DEFAULT: '0 2px 8px rgba(0, 0, 0, 0.1)',
-      lg: '0 4px 16px rgba(0, 0, 0, 0.15)',
-      xl: '0 8px 36px rgba(0, 0, 0, 0.2)',
-    },
     borderColor: {
       DEFAULT: 'hsl(220, 10%, var(--lighter))',
       strong: 'hsl(220, 10%, var(--strong))',
