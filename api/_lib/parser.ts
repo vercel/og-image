3594626641e70e2d4303c4f21713b984098c6076
Sam Bell <me@sam-bell.com>
2021-08-22 12:10:15 +0100

    Add new images to API

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -52,8 +52,8 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
     const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+        ? 'https://og-image.stockora.com/stockora-black.svg'
+        : 'https://og-image.stockora.com/stockora.svg';
 
     if (!images || !images[0]) {
         return [defaultImage];
