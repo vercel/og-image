4cc844aa7b24446ee2f5ed6baa1fb31b1bfe7303
Steven <steven@ceriously.com>
2022-09-06 18:49:56 -0400

    Update the Next.js logo

--- web/index.ts
+++ web/index.ts
@@ -146,14 +146,14 @@ const markdownOptions: DropdownOption[] = [
 
 const imageLightOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
+    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-light-background.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
 
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
+    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-dark-background.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
 ];
 
