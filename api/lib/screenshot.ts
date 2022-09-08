cf108d37dab7528eda2f91ff8b6d50eb5ee706fc
Jonas Daniels <jonas.daniels@outlook.com>
2022-09-08 12:01:17 -0700

    speed up and trace timestamps for generation

--- api/lib/screenshot.ts
+++ api/lib/screenshot.ts
@@ -1,28 +1,51 @@
 import chrome from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
+import puppeteer, {
+  BrowserConnectOptions,
+  BrowserLaunchArgumentOptions,
+  LaunchOptions,
+} from "puppeteer-core";
+
+const isDev = !process.env.AWS_REGION;
+
+type PuppeteerLaunchOptions = LaunchOptions &
+  BrowserLaunchArgumentOptions &
+  BrowserConnectOptions;
 
 export default async function screenshot(url: string) {
-  const options = process.env.AWS_REGION
+  console.time("screenshot-e2e");
+  console.time("browser-launch");
+  const options: PuppeteerLaunchOptions = isDev
     ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      }
-    : {
         args: [],
+        devtools: false,
         executablePath:
           process.platform === "win32"
             ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
             : process.platform === "linux"
             ? "/usr/bin/google-chrome"
             : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      }
+    : {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+        devtools: false,
       };
 
   const browser = await puppeteer.launch(options);
+  console.timeEnd("browser-launch");
+  console.time("page-setup");
   const page = await browser.newPage();
   // og image is supposed to be 1.9:1 (1200x630)
   // twitter image is supposed to be 2:1 (1200x600), but works fine with 1200x630, too
   await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 2 });
-  await page.goto(url, { waitUntil: "networkidle0" });
-  return await page.screenshot({ type: "png" });
+  console.timeEnd("page-setup");
+  console.time("page-load");
+  await page.goto(url, { waitUntil: "networkidle2" });
+  console.timeEnd("page-load");
+  console.time("take-screenshot");
+  const screenshot = await page.screenshot({ type: "png" });
+  console.timeEnd("take-screenshot");
+  console.timeEnd("screenshot-e2e");
+  return screenshot;
 }
